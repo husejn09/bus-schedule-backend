f995eaa2b7187a6fbac42e5809f54212f38ebfa3
Husejn Hasanovic <husejn138@outlook.com>
2024-09-12 20:44:00 +0200

    Removed and combined functions

--- public/script.js
+++ public/script.js
@@ -9,17 +9,19 @@ document
     document.querySelector(".navbar-list").classList.remove("active");
   });
 
-
+/* Event listener for button to stay clicked */
 document.getElementById("linija-search-id").addEventListener("click", function(){
   this.classList.toggle("clicked")
 })
 
+/* Event listener for showing routes */
 document.getElementById("linija-search-id").addEventListener("click", function(){
   const routesDiv = document.getElementById("routes-main");
   routesDiv.classList.toggle("routes-visible");
   routesDiv.classList.toggle("routes");
 })
 
+/* Function for showing hr on button click - called in HTML */ 
 function hrShow() {
   let m = document.getElementById("bottom_hr");
 
@@ -29,36 +31,25 @@ function hrShow() {
     m.style.display = "block";
   }
 }
-/*
-function routeShow(){
-  let n = document.getElementById("routes-main");
-  if (n.style.display === "block") {
-    n.style.display = "none";
-  } else {
-    n.style.display = "block";
-  } 
-}*/
 
-function hrShow2() {
-    let n = document.getElementById("linija_C");
-  
-    if (n.style.display === "block") {
-      n.style.display = "block";
-    } else if(document.getElementById("linija_C"). style.display === 'none'){
-      n.style.display = "block";
-    }
-    else n.style.display === "block"
+function showInfo(){
+  let n = document.getElementById("choose-div-id");
+  let x = document.getElementById("main-card-id");
+
+
+  if (n.style.display === "none" ) {
+    n.style.display = "flex";
+  } else {
+    n.style.display = "flex";
   }
 
-function scrollToDiv() {
-  let scroll = document.getElementById("scroll_id");
-  scroll.scrollIntoView({ block: "center", behavior: "smooth" });
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "flex";
+  }
 }
 
-function scrollToDiv2() {
-    let scroll = document.getElementById("directionSelect");
-    scroll.scrollIntoView({ block: "center", behavior: "smooth" });
-  }
 
 function linijaButton() {
   let n = document.getElementById("card_id");
@@ -80,45 +71,6 @@ function linijaButton() {
   }, 150);
 }
 
-
-// Uzimanje podataka start end stanice
-async function startEndStation(route_id) {
-  try {
-    const response = await fetch(
-      `http://localhost:3000/route_stations/${route_id}`
-    );
-    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-    return await response.json();
-  } catch (error) {
-    console.error("Error fetching start and end station names:", error);
-  }
-}
-
-// Prikaz podataka start i end stanice
-async function prikazStartEnd(route_id) {
-  const data = await startEndStation(route_id);
-
-  if (!data) {
-    console.error("No station data found");
-    return;
-  }
-  const startStation = data.prvaStanica;
-  const endStation = data.zadnjaStanica;
-  const startS = data.prvaStanica;
-
-  if (startStation) {
-    document.getElementById("start_stanica").textContent = startStation.station_name;
-    document.getElementById("station_name").textContent = startStation.station_name;
-  } else {
-    document.getElementById("start_stanica").textContent = "Start station not found";
-  }
-
-  if (endStation) {
-    document.getElementById("end_stanica").textContent = endStation.station_name;
-  } else {
-    document.getElementById("end_stanica").textContent = "End station not found";
-  }
-}
 
 
 // Globalna promenljiva za cuvanje ID-a rute
@@ -147,9 +99,7 @@ async function handleRouteClick(routeId) {
 
     // Ako imamo validan ID rute, pozivamo prikaz funkcije
     if (route_id) {
-        await prikazStartEnd(route_id);
-        await prikazDepartureTime(route_id, day_of_week);
-        await getVrijeme();
+        await handleDepartureTimes(route_id, day_of_week);
 
     } else {
         console.log("Greška prilikom fetch-a");
@@ -158,10 +108,11 @@ async function handleRouteClick(routeId) {
 
 // Dodavanje event listenera za sve dugmadi odjednom
 const routeButtons = [1, 3, 5, 7, 9];
-routeButtons.forEach(routeId => {
-    document.getElementById(routeId.toString()).addEventListener("click", function() {
-        handleRouteClick(routeId);
-    });
+routeButtons.forEach((routeId) => {
+  document.getElementById(routeId.toString()).addEventListener("click", function () {
+    showInfo();
+    handleRouteClick(routeId);
+  });
 });
 
 
@@ -178,48 +129,10 @@ async function departureTime(route_id, day_of_week) {
   }
 }
 
-// Funkcija za prikaz vremena polaska
-async function prikazDepartureTime(route_id, day_of_week) {
-    try {
-        const response = await fetch(
-            `http://localhost:3000/schedules/${route_id}/${day_of_week}`
-        );
-        const data = await response.json();
-
-        if (!data || data.length === 0) {
-            console.error("No departure time found");
-            return;
-        }
-
-        const paraSelect = document.getElementById("start_time");
-        const timeSelect = document.getElementById("timeSelect");
-        timeSelect.innerHTML = "";
-
-
-        data.forEach((time, index) => {
-            const option = document.createElement("option");
-            option.value = time.departure_time; 
-            option.textContent = time.departure_time;
-            if (index === 0) {
-                option.selected = true;
-            }
-
-            timeSelect.appendChild(option);
-        });
-        paraSelect.innerHTML = "";
-        paraSelect.textContent = data[0].departure_time;
-
-        
-    } catch (error) {
-        console.error("Error fetching departure times:", error);
-    }
-}
-
 
 // Event listener za odabir pravca (Outbound/Inbound)
 document.getElementById("directionSelect").addEventListener("change", async function() {
     const selectedDirection = this.value; // 1 za Outbound, 2 za Inbound
-    let x = document.getElementById('timeSelect').value;
 
     let route_id = selectedDirection == 1 ? globalRouteId : globalRouteId + 1;
     let day_of_week = document.getElementById("daySelect").value;
@@ -231,9 +144,8 @@ document.getElementById("directionSelect").addEventListener("change", async func
     } else {
         day_of_week = "Sunday";
     }
-    await prikazStartEnd(route_id);
-    await prikazDepartureTime(route_id, day_of_week);
-    
+   // await prikazStartEnd(route_id);
+    await handleDepartureTimes(route_id, day_of_week);
     
 });
 
@@ -242,7 +154,6 @@ document.getElementById("daySelect").addEventListener("change", async function()
     const selectedDirection = document.getElementById("directionSelect").value;
     let route_id = selectedDirection == 1 ? globalRouteId : globalRouteId + 1;
     let day_of_week = this.value;
-    let x = document.getElementById('timeSelect').value;
 
     if (day_of_week == 1) {
         day_of_week = "Monday";
@@ -252,25 +163,130 @@ document.getElementById("daySelect").addEventListener("change", async function()
         day_of_week = "Sunday";
     }
     
-    await prikazStartEnd(route_id);
-    await prikazDepartureTime(route_id, day_of_week);
-    
+   // await prikazStartEnd(route_id);
+    await handleDepartureTimes(route_id, day_of_week);
 });
 
-// Event listener za promenu vremena polaska
-document.getElementById("timeSelect").addEventListener("change", async function() {
-    const selectedDirection = document.getElementById("directionSelect").value;
-    let route_id = selectedDirection == 1 ? globalRouteId : globalRouteId + 1;
-    let n = document.getElementById('start_time');
-    let x = document.getElementById("timeSelect").value;
-    n.innerHTML = x;
-    
-});
+
+
+async function startEndStation(route_id) {
+  try {
+      const response = await fetch(
+      `http://localhost:3000/route_stations/${route_id}`
+      );
+      if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+      return await response.json();
+  } catch (error) {
+      console.error("Error fetching start and end station names:", error);
+  }
+}
+
+async function handleDepartureTimes(route_id, day_of_week) {
+  try {
+      const departureTimes = await departureTime(route_id, day_of_week); 
+
+      if (!departureTimes || departureTimes.length === 0) {
+          console.error("No departure times found");
+          return;
+      }
+
+      const stationContainer = document.getElementById("stationContainer");
+      stationContainer.innerHTML = ""; 
+
+      for (const time of departureTimes) {
+          const cardDiv = document.createElement("div");
+          cardDiv.className = "card-info";
+
+          const endTime = await calculateTotalTravelTime(route_id, time.departure_time); 
+          const stationShow = await startEndStation(route_id);
+
+          const startTime = time.departure_time.slice(0,5);
+          cardDiv.innerHTML = `
+              <div class="time-info">
+                  <p id="start-time">${startTime}</p>
+                  <p id="end-time">${endTime}</p>
+              </div>
+
+              <div class="dots" id="dots-id">
+                  <span class="dot" id="dot1"></span>
+                  <hr class="info-hr">
+                  <span class="dot" id="dot2"></span>
+              </div>
+
+              <div class="stanica-info">
+                  <p id="start-station">${stationShow.prvaStanica.station_name}</p>
+                  <p id="end-station">${stationShow.zadnjaStanica.station_name}</p>
+              </div>
+
+              <div class="r-button-class">
+                  <button class="r-buttons-card">BAM 03.00</button>
+              </div>
+              <div>
+                  <img src="assets/down-arrow.png" alt="arrow-logo" class="arrow-logo" id="arrow-id">
+              </div>
+          `;
+
+          stationContainer.appendChild(cardDiv); // Append the new card to the container
+      }
+  } catch (error) {
+      console.error("Error fetching and displaying departure times:", error);
+  }
+}
+
+
+async function showTravelInfo(route_id) {
+  try {
+      const response = await fetch(
+        `http://localhost:3000/route-stations-show/next/${route_id}`
+      );
+      if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+      return await response.json();
+    } catch (error) {
+      console.error("Error fetching departure time:", error);
+    }
+}
+
+// Function to calculate total travel time (based on your existing travel calculation logic)
+async function calculateTotalTravelTime(route_id, departure_time) {
+  try {
+      const stationData = await showTravelInfo(route_id); 
+
+      if (!stationData || stationData.length === 0) {
+          console.error("No station data found");
+          return "N/A";
+      }
+
+      // Initialize start time with the departure time
+      let currentTime = new Date();
+      const initialTimeParts = departure_time.split(':').map(Number);
+      currentTime.setHours(initialTimeParts[0], initialTimeParts[1], initialTimeParts[2]);
+
+      // Loop through each station and add travel time to the current time
+      stationData.forEach(station => {
+          currentTime.setSeconds(currentTime.getSeconds() + station.travelTime);
+      });
+
+      // Format the calculated time into HH:MM:SS format for display
+      const hours = String(currentTime.getHours()).padStart(2, '0');
+      let minutes = String(currentTime.getMinutes());
+      const seconds = String(currentTime.getSeconds());
+
+      if (seconds >= 15){
+        minutes = String(currentTime.getMinutes()+1);
+      }
+      minutes = String(minutes).padStart(2, '0');
+
+      return `${hours}:${minutes}`; // This will be displayed as the end time
+  } catch (error) {
+      console.error("Error calculating total travel time:", error);
+      return "N/A";
+  }
+}
 
 
 // Funkcija za prikaz vremena polaska
 
-async function prikaz(route_id) {
+async function getTravelTime(route_id) {
     try {
         const response = await fetch(
           `http://localhost:3000/route-stations/next/${route_id}`
@@ -282,14 +298,9 @@ async function prikaz(route_id) {
       }
 }
 
-function getVrijeme(){
-    let vrijeme = document.getElementById('timeSelect').value;
-    return vrijeme;
-}
-
-async function prikazData(route_id) {
+async function showTravelTime(route_id, departure_time) {
     try {
-        const data = await prikaz(route_id);
+        const data = await getTravelTime(route_id);
 
         if (!data || data.length === 0) {
             console.error("No departure time found");
@@ -335,7 +346,7 @@ async function prikazData(route_id) {
 
             const startTime = formatTime(currentTime);
             stationDiv.innerHTML = `
-                <div class="img_div" id="img_id${index + 2}"><img class="bus_icon" alt="bus_icon" src="transport_15845536.png"></div>
+                <div class="img_div" id="img_id${index + 2}"><img class="bus_icon" alt="bus_icon" src="assets/bus-icon-black.png"></div>
                 <div class="time_div" id="time_id${index + 2}"><p id="start_time${index + 2}">${startTime}</p></div>
                 <div class="arrow_div" id="arrow_id${index + 2}">&rarr;</div>
                 <div class="arrv_station" id="arrv_station_id${index + 2}"><p id="station_name${index + 2}">${station.to}</p></div>
